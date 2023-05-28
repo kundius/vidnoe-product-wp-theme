f43ad648679f5fa2f619aff9060c186a9c4a99e4
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-05-29 02:23:16 +0300

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -13,8 +13,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset');?>" />
 <link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/favicon.ico" type="image/x-icon">
-<meta name="yandex-verification" content="" />
 <meta name="cmsmagazine" content="ef9002bde22164e1bb8ca1b1793cf47b" />
+<meta name="yandex-verification" content="beade4ef55bf0c59" />
 <meta itemprop="name" content="<?php bloginfo('blogname') ?>" />
 <meta itemprop="description" content="<?php bloginfo('description') ?>" />
 
