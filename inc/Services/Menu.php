e0763c72749d8d6865f12e0653cf74feeabf471a
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-05-29 03:05:52 +0300

    Update Menu.php

--- inc/Services/Menu.php
+++ inc/Services/Menu.php
@@ -40,6 +40,7 @@ class Menu implements Service
     {
         $nav_menu = [
             'menu-main' => 'Основное меню',
+            'menu-footer' => 'Меню в подвале',
             'menu-sitemap' => 'Карта сайта',
         ];
         register_nav_menus($nav_menu);
